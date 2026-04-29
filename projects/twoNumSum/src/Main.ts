2f73573c35d4a2ab101e50257bfb56e4ff22ab6e
Ed Salt <ed.salt@outlook.com>
2026-04-29 17:47:25 +0100

    DCP#1 in .py + .ts

---
+++ projects/twoNumSum/src/Main.ts
@@ -0,0 +1,20 @@
+/**
+ * Problem:
+ * Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+ *
+ * For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+ *
+ * Bonus:
+ * Can you do this in one pass?
+ */
+
+function twoSum(nums: number[], k: number): boolean
+{
+    for (const num of nums)
+    {
+        if (nums.includes(k - num)) { return true; }
+    }
+    return false;
+}
+
+console.log(twoSum([10, 15, 3, 7], 17));
